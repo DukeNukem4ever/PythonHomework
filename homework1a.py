b06c6025c4aaa2f97a55c151be64b383a76efe29
DukeNukem4ever <31654733+DukeNukem4ever@users.noreply.github.com>
2017-09-27 16:46:21 +0300

    ДЗ
    Залил ДЗ.

---
+++ homework1a.py
@@ -0,0 +1,10 @@
+print ("Введите число А")
+a = int(input())
+print ("Введите число B")
+b = int(input())
+print ("Введите число C")
+c = int(input())
+if a * b == c:
+        print("Умножение имеет смысл")
+else:
+        print("Умножение не имеет смысла")
---
+++ homework1b.py
@@ -0,0 +1,11 @@
+print("Введите число A")
+a = int(input())
+print("Введите число B")
+b = int(input())
+print("Введите число C")
+c = int(input())
+if a / b == c:
+        print("Деление имеет смысл")
+else:
+        print("Деление не имеет смысла")
+
