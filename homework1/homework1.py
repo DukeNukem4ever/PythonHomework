da6fcb239960859ed5b8cdd7e8db6ac5996b58e4
DukeNukem4ever <31654733+DukeNukem4ever@users.noreply.github.com>
2017-11-15 11:06:03 +0300

    Add files via upload

---
+++ homework1/homework1.py
@@ -0,0 +1,22 @@
+print ("Введите число А")
+a = int(input())
+print ("Введите число B")
+b = int(input())
+print ("Введите число C")
+c = int(input())
+if a * b == c:
+        print("Умножение имеет смысл")
+else:
+        print("Умножение не имеет смысла")
+
+#print("Введите число A")
+#a = int(input())
+#print("Введите число B")
+#b = int(input())
+#print("Введите число C")
+#c = int(input())
+#if a / b == c:
+#        print("Деление имеет смысл")
+#else:
+#        print("Деление не имеет смысла")
+
