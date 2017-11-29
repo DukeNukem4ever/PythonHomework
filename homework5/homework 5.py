615268242426ff979d629373b6462406ac80847f
DukeNukem4ever <31654733+DukeNukem4ever@users.noreply.github.com>
2017-11-30 01:03:14 +0300

    Add files via upload

---
+++ homework5/homework 5.py
@@ -0,0 +1,53 @@
+#x = input("Введите слово: ")
+#print(x)
+#str = x
+#word_length = len(x)
+#letters = []
+#a = 0
+#for j in (x):
+#    suffix = "tur";
+#    letters.append (j)
+#    if word_length == 0:
+#        break
+#    else:
+#        print ("Введите ещё слово")
+#print string.endswith(suffix, 2, 4)        
+
+#word = input("Введите слово: ")
+#print (word)
+#l = []
+#while bool(word) == False:
+#    try:
+#        print(l)
+#        break
+#    except:
+#        break
+#while bool(word) == True:
+#    try:
+#        print ("Введите ещё слово: ")
+#        l.append[word]
+#    except:
+#        break
+
+word = input("Введите слово: ")
+print (word)
+res = []
+while bool(word) == False:
+    if len(word) == 0:
+        print(res)
+        break
+    else:
+        res.append(word)
+        print ("Введите слово: ")
+        while bool(word) == True:
+            jump(False)
+            continue
+
+#name = raw_input("What is your name?")
+#while name == "":
+#    print ("Hey! You didn't enter a name!")
+#    name = raw_input("Okay, REALLY tell me your name this time:")
+#if name == ("spam") or name == ("eggs"):
+#    print ("A moose once bit my sister!")
+#else:
+#    print ("Nice to meet you, %s" % name
