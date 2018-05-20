dd348d79bae65a360719faa24444eb54fc047a82
DukeNukem4ever <31654733+DukeNukem4ever@users.noreply.github.com>
2018-05-20 22:39:53 +0300

    Update hw13.py

--- homework13/hw13.py
+++ homework13/hw13.py
@@ -1,24 +1,23 @@
 import os
 
-def program():
-    amount = 0
-    prev1 = None
-    prev2 = None
-    start_path = '.'
+def stat(prev1,prev2, amount, start_path):
     for root, dirs, files in os.walk(start_path):
-        print('Где мы сейчас:', root)
-        print('Количество файлов:')
+        print()
+        print('ГДЕ МЫ СЕЙЧАС:', root)
+        print()
+        print('ФАЙЛЫ:')
+        print()
         for dire in dirs:
+            print(dire)
             if dire != prev1:
-                print(dire)
                 amount += 1
             prev1 = dire
         for file in files:
+            print(file)
             if file != prev2:
-                print(file)
                 amount += 1
             prev2 = file
     print(amount)
     return amount
 
-program()
+stat(None, None, 0, '.')
